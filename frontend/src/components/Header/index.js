51fb7f5d607a4580fdf1c6b20c2225328d31abef
Kauã Ferreira Borba <kaua.borba03@gmail.com>
2022-10-16 00:05:24 -0300

    finalyday

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import './style.css'
+
+function Header(){
+    return (
+        <header className="header">
+            <h2>E-kicky</h2>
+
+            <ul>
+                <li>Cadastrar</li>
+                <li>Login</li>
+            </ul>
+        </header>
+    )   
+}
+
+export default Header
